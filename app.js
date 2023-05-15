293a53cd1ba0b4a7ed214f26a25714aee467f4e1
palwashawahidkhan <132895734+palwashawahidkhan@users.noreply.github.com>
2023-05-15 22:29:59 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+//chapter1 of JS
+
+alert ("Error! please enter a valid password")
+alert ("Welcome to JS Land... \nHappy Coding!" )
+alert ("Welcome to JS Land...")
+alert ("Happy Coding!")
+alert ("hello i can run JS through  my web browser's console")
+console.log("hello i can run JS through my web browser's console")
+
+
+//chapter2 of JS
+
+let username
+let myname="palwasha khan"
+let = "hello world"
+alert ("hello world")
+
+
+let stdname = "khan";
+alert("khan");
+let age = "15 year old";
+alert ("15");
+
+let coursename = "certified mobile application";
+alert ("web and mobile application");
+
+// let item = "PIZZA";  
+// alert ("item+"\n" +item[0]+ item[1]+ item+ item[2]+ item[3]+ "\n" + item[0] + item[1] + item[2] +
+//  "\n" + item[0] + item[1]"\n" +item[0]");
+
+alert("my email address is palwashawahi163@gmail.com")
+alert ("I am trying to learn from the Book A Smarter way to learn JavaScript")
+document.write ("Yah! I can write HTML content through JavaScript")
+
+
+
+
+
+
+
